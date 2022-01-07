73138ae3b486293b4593849cf1f89eb97a73dbed
Matthew Ballance <matt.ballance@gmail.com>
2022-01-07 14:25:43 -0800

    Basic VPI BFM support in place
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/tblink_bfms/generators/gen_verilog.py
+++ src/tblink_bfms/generators/gen_verilog.py
@@ -8,6 +8,7 @@ from _io import StringIO
 from tblink_bfms.generators.gen_base import GenBase
 from tblink_rpc_utils.gen_utils import GenUtils
 from tblink_rpc_utils.output import Output
+from tblink_rpc_utils.type_spec import TypeSpec, TypeKind
 
 
 class GenVerilog(GenBase):
@@ -68,19 +69,45 @@ class GenVerilog(GenBase):
         out.dec_ind()
         
         out.println()
-        out.println("// TODO: method-lookup")
+        out.println("// Lookup method handles")
+        self.gen_method_lookup(out, iftype, is_mirror)
         out.println()
-        out.println("// TODO: event-processing loop")
+        out.println("// Event-processing loop")
         out.println("while (1) begin : invoke_loop")
+        out.inc_ind()
+        out.println("reg[31:0] ret;")
+        out.println("reg[63:0] method;")
+        out.println("reg[63:0] call_id;")
+        out.println("reg[63:0] params;")
+        out.println()
+        out.println("ret = $tblink_rpc_InterfaceInstWrapper_nextInvokeReq(")
+        out.inc_ind()
+        out.inc_ind()
+        out.println("m_ifinst,")
+        out.println("method,")
+        out.println("call_id,")
+        out.println("params);")
+        out.dec_ind()
+        out.dec_ind()
+        out.println()
+        out.println("$display(\"ret=%0d\", ret);")
+        out.println()
+        out.println("if (ret == 1) begin : invoke_avail")
         out.inc_ind()
         out.println("reg[31:0] id;")
         out.println()
-        out.println("case (m_id)")
-        out.println("32'hFFFF_FFFF: begin")
-        out.inc_ind()
-        out.println("@(m_ev);")
-        out.dec_ind()
-        out.println("end")
+        out.println("id = $tblink_rpc_IMethodType_id(method);")
+        out.println()
+        out.println('$display("id=%0d", id);')
+        out.println()
+        
+        out.println("case (id)")
+        for i,m in enumerate(iftype.methods):
+            if is_mirror and not m.is_export or not is_mirror and m.is_export:
+                if m.is_blocking:
+                    self.gen_invoke_b_case(out, i, m)
+                else:
+                    self.gen_invoke_nb_case(out, i, m)
         out.println("default: begin")
         out.inc_ind()
         out.println("$display(\"TbLink Error: %m unknown call-id %0d\", m_id);")
@@ -89,6 +116,12 @@ class GenVerilog(GenBase):
         out.dec_ind()
         out.println("end")
         out.println("endcase")
+        out.dec_ind()
+        out.println("end else begin // No message available")
+        out.inc_ind()
+        out.println("@(m_ev);")
+        out.dec_ind()
+        out.println("end")
         out.dec_ind()
         out.println("end")
         out.dec_ind()
@@ -122,6 +155,12 @@ class GenVerilog(GenBase):
         out.dec_ind()
         pass
     
+    def gen_method_lookup(self, out, iftype, is_mirror):
+        for m in iftype.methods:
+            if not m.is_export and not is_mirror or m.is_export and is_mirror:
+                out.println("m_%s = $tblink_rpc_IInterfaceType_findMethod(m_iftype, \"%s\");" % (
+                    m.name, m.name))
+
     def gen_define_type(self, out, iftype):
         g_util = GenUtils(vlog=True)
         out.println("function reg[63:0] define_type(input reg[63:0] ep);")
@@ -330,6 +369,101 @@ class GenVerilog(GenBase):
             out.println("endtask")
         else:
             out.println("endfunction")
+            
+    def gen_invoke_b_case(self, out, i, m):
+        out.println("%d : begin : _invoke_%s" % (i, m.name))
+        out.inc_ind()
+        out.println("reg[63:0]    retval_h;")
+        out.println("reg[63:0]    param_h;")
+        
+        if m.rtype is not None:
+            out.println("%s retval;" % self._type2str(m.rtype))
+
+        # Declare variables for parameters        
+        for p in m.params:
+            out.println("%s _param_%s;" % (self._type2str(p[1]), p[0]))
+        
+        out.println("retval_h = 0;")
+
+        # Fetch parameter values        
+        for i,p in enumerate(m.params):
+            out.println("param_h = $tblink_rpc_IParamValVec_at(params, %d);" % i);
+            if p[1].kind == TypeKind.Int:
+                if p[1].is_signed:
+                    out.println("_param_%s = $tblink_rpc_IParamValInt_val_s(param_h);" % p[0])
+                else:
+                    out.println("_param_%s = $tblink_rpc_IParamValInt_val_u(param_h);" % p[0])
+            else:
+                raise Exception("Unsupported parameter %s in method %s" % (
+                    str(p[1].kind), p[0]))
+                
+        out.println()
+       
+
+        if len(m.params) == 0 and m.rtype is None:
+            out.println("%s;" % m.name)
+        else:        
+            out.println("%s(" % m.name)
+            out.inc_ind()
+            out.inc_ind()
+            for i,p in enumerate(m.params):
+                if i+1 < len(m.params):
+                    out.println("_param_%s," % p[0])
+                else:
+                    out.println("_param_%s" % p[0])
+            out.dec_ind()
+            out.println(");")
+            out.dec_ind()
+
+        out.println()
+        out.println("$tblink_rpc_InterfaceInstWrapper_invoke_rsp(")
+        out.inc_ind()
+        out.inc_ind()
+        out.println("m_ifinst,")
+        out.println("call_id,")
+        out.println("retval_h")
+        out.dec_ind()
+        out.println(");")
+        out.dec_ind()
+
+        out.dec_ind()
+        out.println("end")
+        pass
+            
+    def gen_invoke_nb_case(self, out, i, m):
+        out.println("%d : begin : %s" % (i, m.name))
+        out.inc_ind()
+        out.dec_ind()
+        out.println("end")
+        pass
+    
+    _int_type_u_m = {
+        8: 'reg[7:0]',
+        16: 'reg[15:0]',
+        32: 'reg[31:0]',
+        64: 'reg[63:0]'}
+    
+    _int_type_s_m = {
+        8: 'reg signed[7:0]',
+        16: 'reg signed[15:0]',
+        32: 'reg signed[31:0]',
+        64: 'reg signed[63:0]'}
+    
+    def _type2str(self, type_s : TypeSpec):
+        if type_s.kind == TypeKind.Int:
+            if not type_s.is_signed:
+                ret = GenVerilog._int_type_s_m[type_s.width]
+            else:
+                ret = GenVerilog._int_type_u_m[type_s.width]
+        elif type_s.kind == TypeKind.Bool:
+            ret = "reg"
+        elif type_s.kind == TypeKind.Str:
+            ret = "string"
+        else:
+            raise Exception("Unsupported typespec %s" % str(type_s.kind))
+            pass
+        
+        return ret
 
     def tblink_gen(self, iftype, is_mirror, kind=None, **kwargs):
         ret_s = StringIO()
